4ca9cfb9b537e719c73b46dc824c9189d686433a
Amr Swalha <amr.swalha.code@outlook.com>
2019-03-31 21:35:00 +0400

    basic work

--- Basics/Program.cs
+++ Basics/Program.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +13,16 @@ namespace Basics
     {
         static void Main(string[] args)
         {
+            var credentials =
+                new BasicAWSCredentials(ConfigurationManager.AppSettings["accessId"], ConfigurationManager.AppSettings["secertKey"]);
+            using (AmazonS3Client client = new AmazonS3Client(credentials, Amazon.RegionEndpoint.USWest1))
+            {
+                foreach (var bucket in client.ListBuckets().Buckets)
+                {
+                    Console.WriteLine(bucket.BucketName + "   " + bucket.CreationDate.ToShortDateString());
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
