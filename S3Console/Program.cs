d882da0f3384575dd7821f6d4419e323bbc21629
Amr Swalha <amr.swalha.code@outlook.com>
2019-04-03 21:59:01 +0400

    Adding S3 upload lecture

--- S3Console/Program.cs
+++ S3Console/Program.cs
@@ -1,9 +1,4 @@
-﻿using Amazon.Runtime;
-using Amazon.S3;
-using Amazon.S3.Model;
-using Amazon.S3.Util;
-using System;
-using System.Configuration;
+﻿using System;
 
 namespace S3Console
 {
@@ -11,24 +6,8 @@ namespace S3Console
     {
         static void Main(string[] args)
         {
-            var credentials =
-               new BasicAWSCredentials(ConfigurationManager.AppSettings["accessId"], ConfigurationManager.AppSettings["secertKey"]);
-            using (AmazonS3Client client = new AmazonS3Client(credentials, Amazon.RegionEndpoint.USWest1))
-            {
-                if (AmazonS3Util.DoesS3BucketExist(client, "myapp564"))
-                {
-                    Console.WriteLine("Bucket already exists");
-                }
-                else
-                {
-                    var bucket = new PutBucketRequest { BucketName = "myapp564", UseClientRegion = true };
-                    var bucketResponsoe = client.PutBucket(bucket);
-                    if (bucketResponsoe.HttpStatusCode == System.Net.HttpStatusCode.OK)
-                    {
-                        Console.WriteLine("Bucket Created Successfully");
-                    }
-                }
-            }
+           S3BucketOperations s3BucketOperations = new S3BucketOperations();
+            s3BucketOperations.UploadFile();
             Console.ReadLine();
         }
     }
---
+++ S3Console/S3BucketOperations.cs
@@ -0,0 +1,60 @@
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+using Amazon.S3.Util;
+using System;
+using System.Configuration;
+using System.Linq;
+
+namespace S3Console
+{
+    public class S3BucketOperations : IDisposable
+    {
+        AmazonS3Client client;
+        const string bucketName = "myapp564";
+        public BasicAWSCredentials credentials =
+              new BasicAWSCredentials(ConfigurationManager.AppSettings["accessId"], ConfigurationManager.AppSettings["secertKey"]);
+        public S3BucketOperations()
+        {
+            client = new AmazonS3Client(credentials, Amazon.RegionEndpoint.USWest1);
+        }
+
+        public void CreateBucket()
+        {
+
+            if (AmazonS3Util.DoesS3BucketExist(client, bucketName))
+            {
+                Console.WriteLine("Bucket already exists");
+            }
+            else
+            {
+                var bucket = new PutBucketRequest { BucketName = bucketName, UseClientRegion = true };
+                var bucketResponsoe = client.PutBucket(bucket);
+                if (bucketResponsoe.HttpStatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    Console.WriteLine("Bucket Created Successfully");
+                }
+            }
+        }
+        public void UploadFile()
+        {
+            var transfareUtil = new TransferUtility(client);
+            //transfareUtil.UploadDirectory(AppDomain.CurrentDomain.BaseDirectory + "\\files",bucketName);
+            //transfareUtil.Upload(AppDomain.CurrentDomain.BaseDirectory + "\\test.txt",bucketName);
+            var fileTransferRequest = new TransferUtilityUploadRequest
+            {
+                FilePath = AppDomain.CurrentDomain.BaseDirectory + "\\test.txt",
+                CannedACL = S3CannedACL.PublicRead,
+                 BucketName = bucketName
+            };
+            transfareUtil.Upload(fileTransferRequest);
+            Console.WriteLine("File Uploaded Successfully");
+        }
+
+        public void Dispose()
+        {
+            client.Dispose();
+        }
+    }
+}
