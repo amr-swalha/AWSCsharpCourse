4037f833c62cee0d2669cf06bb50bbb6be0c73a1
Amr Swalha <amr.swalha.code@outlook.com>
2019-04-05 16:59:16 +0400

    Adding pre Presigned Url To Share Objects

--- S3Console/S3BucketOperations.cs
+++ S3Console/S3BucketOperations.cs
@@ -5,19 +5,26 @@ using Amazon.S3.Transfer;
 using Amazon.S3.Util;
 using System;
 using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace S3Console
 {
     public class S3BucketOperations : IDisposable
     {
+        #region Fields
         AmazonS3Client client;
         const string bucketName = "myapp564";
         public BasicAWSCredentials credentials =
               new BasicAWSCredentials(ConfigurationManager.AppSettings["accessId"], ConfigurationManager.AppSettings["secertKey"]);
+        TransferUtility transferUtil;
+        #endregion
+
         public S3BucketOperations()
         {
             client = new AmazonS3Client(credentials, Amazon.RegionEndpoint.USWest1);
+            transferUtil = new TransferUtility(client);
         }
 
         public void CreateBucket()
@@ -39,7 +46,6 @@ namespace S3Console
         }
         public void UploadFile()
         {
-            var transfareUtil = new TransferUtility(client);
             //transfareUtil.UploadDirectory(AppDomain.CurrentDomain.BaseDirectory + "\\files",bucketName);
             //transfareUtil.Upload(AppDomain.CurrentDomain.BaseDirectory + "\\test.txt",bucketName);
             var fileTransferRequest = new TransferUtilityUploadRequest
@@ -48,10 +54,43 @@ namespace S3Console
                 CannedACL = S3CannedACL.PublicRead,
                  BucketName = bucketName
             };
-            transfareUtil.Upload(fileTransferRequest);
+            transferUtil.Upload(fileTransferRequest);
             Console.WriteLine("File Uploaded Successfully");
         }
-
+        public async Task DownloadFileAsync()
+        {
+            string content = string.Empty;
+            GetObjectRequest request = new GetObjectRequest {
+                BucketName = bucketName,
+                Key = "test.txt"
+            };
+            using (GetObjectResponse response = await client.GetObjectAsync(request))
+            {
+                using (Stream responseStream = response.ResponseStream)
+                {
+                    using (StreamReader reader = new StreamReader(responseStream))
+                    {
+                        string contenType = response.Headers["Content-Type"];
+                        content = reader.ReadToEnd();
+                        Console.WriteLine("File Content:");
+                        Console.WriteLine(content);
+                        Console.WriteLine("File Content Type: " + contenType);
+                    }
+                }
+            }
+            Console.WriteLine("File Download Successfully");
+        }
+        public void GeneratePreSignedUrl()
+        {
+            GetPreSignedUrlRequest request = new GetPreSignedUrlRequest {
+                BucketName = bucketName,
+                Key = "test.txt",
+                Expires = DateTime.Now.AddHours(1)
+            };
+            var url = client.GetPreSignedURL(request);
+            Console.WriteLine("Sharing URL");
+            Console.WriteLine(url);
+        }
         public void Dispose()
         {
             client.Dispose();
